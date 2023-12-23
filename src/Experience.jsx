b5d881f8545eb4352ef96cbb542d87070662ec96
John Hash <johnhash@frozen.com>
2023-12-23 19:10:36 +0100

    Init

---
+++ src/Experience.jsx
@@ -0,0 +1,107 @@
+import {
+  ContactShadows,
+  Environment,
+  Float,
+  Html,
+  PresentationControls,
+  Stars,
+  Text,
+  useGLTF,
+} from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+
+export default function Experience() {
+  const [scale, setScale] = useState(1);
+  const [meshPosition, setMeshPosition] = useState([0, -1.2, 0]);
+  const [hover, setHover] = useState(false);
+
+  const mesh = useRef();
+
+  // useFrame((state) => {
+  //   if (hover) {
+  //     state.camera.lookAt(mesh.current.position);
+  //   }
+  //   return null;
+  // });
+
+  const computer = useGLTF(
+    "https://threejs-journey.com/resources/models/macbook_model.gltf"
+  );
+
+  const enterHandle = () => {
+    setScale(1.8);
+    setMeshPosition([mesh.current.position.x, -3, mesh.current.position.z]);
+    // setHover(true);
+  };
+
+  const leaveHandler = () => {
+    setScale(1);
+    setMeshPosition([0, -1.2, 0]);
+    // setHover(false);
+  };
+  return (
+    <>
+      <Environment preset="city" />
+
+      <color args={["#241a1a"]} attach="background" />
+
+      <PresentationControls
+        global
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={{ mass: 2, tension: 400 }}
+        snap={{ mass: 4, tension: 400 }}
+      >
+        <Float rotationIntensity={0.4}>
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={65}
+            color={"#ff6900"}
+            rotation={[0.1, Math.PI, 0]}
+            position={[0, 0.55, -1.15]}
+          />
+          <primitive
+            ref={mesh}
+            object={computer.scene}
+            position={meshPosition}
+            onPointerEnter={enterHandle}
+            onPointerLeave={leaveHandler}
+            scale={scale}
+          >
+            <Html
+              transform
+              wrapperClass="htmlScreen"
+              distanceFactor={1.17}
+              position={[0, 1.56, -1.4]}
+              rotation-x={-0.256}
+            >
+              <iframe src="https://bruno-simon.com/html/" />
+            </Html>
+          </primitive>
+          <Text
+            font="./bangers-v20-latin-regular.woff"
+            fontSize={1}
+            position={[2, 0.75, 0.75]}
+            rotation-y={-1.25}
+            maxWidth={2}
+            textAlign="center"
+          >
+            TEMA LE CV
+          </Text>
+        </Float>
+      </PresentationControls>
+      <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
+      <Stars
+        radius={100}
+        depth={50}
+        count={5000}
+        factor={4}
+        saturation={0}
+        fade
+        speed={1}
+      />
+    </>
+  );
+}
